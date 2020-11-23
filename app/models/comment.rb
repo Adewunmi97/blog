0cab07cd8359f8a8ff5055d27575bbd5cdf68d6b
adewunmi <shonibareadewunmicomfort@gmail.com>
2020-11-23 18:56:25 +0100

    add destroy comments and security for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
-class Article < ApplicationRecord
-  has_many :comments
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+class Comment < ApplicationRecord
+  belongs_to :article
+  validates :body, presence: true
 end
